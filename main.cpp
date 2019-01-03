d4ea99fd5729720afdc7bd9c7e87052b678d07b7
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-01-03 09:54:41 +0800

    添加调试输出

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
     pcapHelper.setPacketFilter("ether proto \\ip");
 
     struct timespec sleepTime {
-        0, 100
+        0, 50
     };
     while (true) {
 //        cout << endl << "begin: " << pcapHelper.getCurTime() << endl;
